ea9cd068e33b150f40561aee38edce0c7bce9ea3
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-22 10:55:01 +0100

    first commit

---
+++ OpenData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenData
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
