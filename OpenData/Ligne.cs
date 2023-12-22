037c399b08cc874d980c4972c03cfaf1d2db539d
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-22 14:56:53 +0100

    Appel API Ok

---
+++ OpenData/Ligne.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace OpenData
+{
+    public class Ligne
+    {
+        // {"id":"SEM:1696","name":"Grenoble, Chavant","lon":5.73233,"lat":45.18502,"zone":"SEM_GENCHAVANT","lines":["SEM:C4","SEM:13"]}
+        [JsonProperty("id")] public string id { get; private set; }
+        [JsonProperty("name")] public string name { get; private set; }
+        [JsonProperty("lon")] public float lon { get; private set; }
+        [JsonProperty("lat")] public float lat { get; private set; }
+        [JsonProperty("zone")] public string zone { get; private set; }
+        [JsonProperty("lines")] public string[] lines { get; private set; }
+
+        public override string ToString()
+        {
+            string line = string.Join(", ", lines);
+            return "Ligne {" +
+                   "id = " + id +
+                   ", name = " + name +
+                   ", lon = " + lon +
+                   ", lat = " + lat +
+                   ", zone = " + zone +
+                   ", lines = " + line +
+                   "}";
+        }
+    }
+}
--- OpenData/Program.cs
+++ OpenData/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using Newtonsoft.Json;
-using OpenData.Properties;
 
 namespace OpenData
 {
@@ -13,22 +12,25 @@ namespace OpenData
         {
             ServicePointManager.SecurityProtocol =
                 SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            // Requete à l'API TAG Grenoble des lignes à proximité d'un point
             // coordonnées GPS le Totem : 45.18486504179179, 5.731181509376984
-            WebRequest request =
-                WebRequest.Create(
-                    "http://data.mobilites-m.fr/api/linesNear/json?x=5.731181509376984&y=45.18486504179179&dist=0&details=true");
+            WebRequest request = WebRequest.Create("http://data.mobilites-m.fr/api/linesNear/json?x=5.731181509376984&y=45.18486504179179&dist=0&details=true");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Console.WriteLine(response.StatusDescription);
+            // Console.WriteLine(response.StatusDescription);
             Stream dataStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(dataStream);
             string responseFromServer = reader.ReadToEnd();
-            Console.WriteLine(responseFromServer);
+            // Console.WriteLine(responseFromServer);
             reader.Close();
             dataStream.Close();
             response.Close();
 
-            List <Ligne> lineReturn = JsonConvert.DeserializeObject<List <Ligne>>(responseFromServer);
-            Console.WriteLine(lineReturn[0]);
+            // Conversion d'un JSON en Objet grace à Newtonsoft.Json (téléchargeable via le gestionnaire de paquet Nuget)
+            List<Ligne> lineList = JsonConvert.DeserializeObject<List<Ligne>>(responseFromServer);
+            foreach (var line in lineList)
+            {
+                Console.WriteLine(line.ToString());
+            }
         }
     }
 }
