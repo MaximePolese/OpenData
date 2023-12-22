7ed2c802dc401d1c0958ccf359be75796ded4df0
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-22 11:51:56 +0100

    Ajout du package NuGet Newtonsoft.Json

--- OpenData/Program.cs
+++ OpenData/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace OpenData
 {
@@ -6,7 +8,21 @@ namespace OpenData
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            ServicePointManager.SecurityProtocol =
+                SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            // coordonnées GPS le Totem : 45.18486504179179, 5.731181509376984
+            WebRequest request =
+                WebRequest.Create(
+                    "http://data.mobilites-m.fr/api/linesNear/json?x=5.731181509376984&y=45.18486504179179&dist=0&details=false");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.WriteLine(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
         }
     }
 }
