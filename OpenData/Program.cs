1ad7b28d47c8caaaa372000a52491d9f4ab18638
maxime.polese <maxime.polese@le-campus-numerique.fr>
2023-12-22 14:57:54 +0100

    refacto

--- OpenData/Program.cs
+++ OpenData/Program.cs
@@ -10,8 +10,8 @@ namespace OpenData
     {
         public static void Main(string[] args)
         {
-            ServicePointManager.SecurityProtocol =
-                SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            
             // Requete à l'API TAG Grenoble des lignes à proximité d'un point
             // coordonnées GPS le Totem : 45.18486504179179, 5.731181509376984
             WebRequest request = WebRequest.Create("http://data.mobilites-m.fr/api/linesNear/json?x=5.731181509376984&y=45.18486504179179&dist=0&details=true");
