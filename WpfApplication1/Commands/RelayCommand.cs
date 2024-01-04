e7f5cc5f02e90eb019963845191ea2fa5e0743e4
maxime.polese <maxime.polese@le-campus-numerique.fr>
2024-01-04 10:24:59 +0100

    MVVM avec WPF

---
+++ WpfApplication1/Commands/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1.Commands
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _toExecute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> toExcecute, Func<object, bool> canExecute = null)
+        {
+            _toExecute = toExcecute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _toExecute(parameter);
+        }
+    }
+}
